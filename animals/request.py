54a911e541300538ad722e932fc5a0fec9402b73
Val Currie <contact@valcuri.email>
2021-07-12 13:37:07 -0500

    get_single_animal

--- animals/request.py
+++ animals/request.py
@@ -23,4 +23,21 @@ ANIMALS = [{
 
 
 def get_all_animals():
+    """get_all_animals returns the entire ANIMALS list"""
     return ANIMALS
+
+def get_single_animal(id):
+    """get_single_animal finds a single animal in ANIMALS using an id
+
+    Args:
+        id (int): the integer id we are looking for in ANIMALS
+
+    Returns:
+        dict: the requested animal
+    """
+    # init an empty variable
+    requested_animal = None
+    for animal in ANIMALS:
+        if animal["id"] == id:
+            requested_animal = animal
+    return requested_animal
