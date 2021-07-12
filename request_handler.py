ff2376e4c056edd9bd594fab9c135c3cd329f0dc
Val Currie <contact@valcuri.email>
2021-07-12 14:28:15 -0500

    added single animal resource

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,35 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals
+from animals import get_all_animals, get_single_animal
 
-
-# QUESTION are we saying the constructor takes the base class as an input?
+# this sets up inheritance
 class HandleRequests(BaseHTTPRequestHandler):
     """HandleRequests is a custom HTTP request handler"""
+
+    def parse_url(self, path):
+        """parse_url separates the path portion of the given url
+
+        Args:
+            path (string): the path that needs parsing
+
+        Returns:
+            tuple: the resource and, if applicable, the id of the specific resource
+        """
+        path_params = path.split("/")
+        # path_params[0] is the empty string before the first slash
+        resource = path_params[1]
+        id = None
+
+
+        try:
+            # if we have a path_params[2] that is int'able, this will succeed
+            id = int(path_params[2])
+        except IndexError:
+            # if we run into an IndexError do nothing
+            pass
+        except ValueError:
+            # same with an ValueError
+            pass
+        return (resource, id)
 
     def _set_headers(self, status):
         """_set_headers is an internal method that sends the proper headers for a given status code
@@ -33,13 +58,17 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set the response code to 'Ok'
         self._set_headers(200)
-        print(self.path)
-        if self.path == "/animals":
-            response = get_all_animals()
-        else:
-            response = []
+        # default response is an empty dict
+        response = {}
+        # ooh! tuple destructuring :)
+        (resource, id) = self.parse_url(self.path)
 
-        # TODO: figure this out a bit
+        if resource == "animals":
+            if id is not None:
+                response = f"{get_single_animal(id)}"
+            else:
+                response = f"{get_all_animals()}"
+
         self.wfile.write(f"{response}".encode())
 
     def do_POST(self):
@@ -64,7 +93,6 @@ def main():
     """
     host = ''
     port = 8088
-    # QUESTION why are these in parens?
     HTTPServer((host, port), HandleRequests).serve_forever()
 
 
